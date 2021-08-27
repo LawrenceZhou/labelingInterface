6f09165bf63f9f368dd0011f61e1909072c9e13a
LawrenceZhou <hmskyijun@gmail.com>
2021-08-27 06:30:10 -0700

    LabelInstance.jsx code clean; remove conventional css

--- templates/static/js/routes.js
+++ templates/static/js/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Route, hashHistory, browserHistory } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import OuterFrame from './components/OuterFrame';
 
 // import more components
 export default (
     <BrowserRouter>
-        <div className="OutContainer">
+
             <Route path='/' component={OuterFrame} />
-        </div>
+
     </BrowserRouter>
 );
