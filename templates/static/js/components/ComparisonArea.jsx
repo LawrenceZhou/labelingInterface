e469397329b1b3d80c316d5affbfb75a8ab6eaef
LawrenceZhou <hmskyijun@gmail.com>
2021-12-08 22:05:47 +0900

    left right key control.

--- templates/static/js/components/ComparisonArea.jsx
+++ templates/static/js/components/ComparisonArea.jsx
@@ -17,27 +17,28 @@ export default class ComparisonArea extends Component{
       dimension: "Arousal",
     };
 
+    this.audio = new Audio(this.state.audioPath);
     this.tickingTimer = this.tickingTimer.bind(this);
     this.handleKeyUpPressed = this.handleKeyUpPressed.bind(this);
     this.handleKeyDownPressed = this.handleKeyDownPressed.bind(this);
-    this.audio = new Audio(this.state.audioPath);
-    
+    this.handleKeyLeftPressed = this.handleKeyLeftPressed.bind(this);
+    this.handleKeyRightPressed = this.handleKeyRightPressed.bind(this);
   }
 
   componentWillReceiveProps(nextProps){
     if(nextProps.boxesPassed !== this.props.boxesPassed){
-      var _boxes = nextProps.boxesPassed;
-        var boxes_top = _boxes.filter(box => box.speaker == nextProps.speaker[0]);
-        var boxes_btm = _boxes.filter(box => box.speaker != nextProps.speaker[0]);
+      var boxes_ = nextProps.boxesPassed;
+        var boxes_top = boxes_.filter(box => box.speaker == nextProps.speaker[0]);
+        var boxes_btm = boxes_.filter(box => box.speaker != nextProps.speaker[0]);
         var _boxes = boxes_btm.concat(boxes_top);
         this.setState({boxes: _boxes});
     }
 
     if(nextProps.speaker !== this.props.speaker){
         this.setState({ speaker: nextProps.speaker });
-        var _boxes = this.state.boxes;
-        var boxes_top = _boxes.filter(box => box.speaker == nextProps.speaker[0]);
-        var boxes_btm = _boxes.filter(box => box.speaker != nextProps.speaker[0]);
+        var boxes_ = this.state.boxes;
+        var boxes_top = boxes_.filter(box => box.speaker == nextProps.speaker[0]);
+        var boxes_btm = boxes_.filter(box => box.speaker != nextProps.speaker[0]);
         var _boxes = boxes_btm.concat(boxes_top);
         this.setState({boxes: _boxes});
     }
@@ -60,9 +61,9 @@ export default class ComparisonArea extends Component{
 
   componentDidMount(){
     this.timerId = setInterval(this.tickingTimer, 30);
-    var _boxes = this.props.boxesPassed;
-    var boxes_top = _boxes.filter(box => box.speaker == this.state.speaker[0]);
-    var boxes_btm = _boxes.filter(box => box.speaker != this.state.speaker[0]);
+    var boxes_ = this.props.boxesPassed;
+    var boxes_top = boxes_.filter(box => box.speaker == this.state.speaker[0]);
+    var boxes_btm = boxes_.filter(box => box.speaker != this.state.speaker[0]);
     var _boxes = boxes_btm.concat(boxes_top);
     this.setState({boxes: _boxes});
     console.log(this.state.boxes);
@@ -95,7 +96,7 @@ export default class ComparisonArea extends Component{
 
   }
 
-    handleKeyDownPressed() {
+  handleKeyDownPressed() {
     console.log("down pressed.");
     var that = this;
     var boxes_ = that.state.boxes;
@@ -110,9 +111,38 @@ export default class ComparisonArea extends Component{
     that.setState({boxes: boxes_});
   }
 
+  handleKeyLeftPressed() {
+    console.log("left pressed.");
+    var that = this;
+    var boxes_ = that.state.boxes;
+    for (var i = boxes_.length - 1; i >= 0; i -- ) {
+      if ( this.state.currentTime * 10 > boxes_[i].x && this.state.currentTime * 10 <= boxes_[i].end && this.state.speaker[0] == boxes_[i].speaker) {
+        if (that.audio.currentTime * 10 - boxes_[i].x < 5 && i != 0 && boxes_[i - 1].speaker == boxes_[i].speaker) {
+          that.audio.currentTime = boxes_[i - 1].x / 10;
+        }else {
+          that.audio.currentTime = boxes_[i].x / 10;
+        }
+        
+        break;
+      }
+    }
+  }
+
+  handleKeyRightPressed() {
+    console.log("right pressed.");
+    var that = this;
+    var boxes_ = that.state.boxes;
+    for (var i = boxes_.length - 1; i >= 0; i -- ) {
+      if ( this.state.currentTime * 10 > boxes_[i].x && this.state.currentTime * 10 <= boxes_[i].end && this.state.speaker[0] == boxes_[i].speaker && i != boxes_.length - 1) {
+        that.audio.currentTime = boxes_[i + 1].x / 10;
+        break;
+      }
+    }
+  }
+
   render() {
     return (
-      <Keyboard target='document' onUp={()=>{this.handleKeyUpPressed()}} onDown={()=>{this.handleKeyDownPressed()}}>
+      <Keyboard target='document' onUp={()=>{this.handleKeyUpPressed()}} onDown={()=>{this.handleKeyDownPressed()}} onLeft={()=>{this.handleKeyLeftPressed()}} onRight={()=>{this.handleKeyRightPressed()}}>
       <Box>
       <Stage width={1600} height={350} >
         <Layer >
