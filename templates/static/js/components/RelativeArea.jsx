dfe7c32e1870ea1abfbd52447aa23d3ab7615879
LawrenceZhou <hmskyijun@gmail.com>
2022-02-08 10:50:24 +0900

    add back without highlight. delete index.

--- templates/static/js/components/RelativeArea.jsx
+++ templates/static/js/components/RelativeArea.jsx
@@ -296,7 +296,7 @@ export default class RelativeArea extends Component{
               
                                 {false && <Text x={1} y={18} width={29} height={this.state.offsetY} fontSize={14} text={this.props.sliderResults[box.sentenceID]} fill='white' strokeWidth={1} align="start" visible={box.sentenceID in this.props.sliderResults} />}
 
-                                {false != 'slider' && <Text x={1} y={18} width={29} height={this.state.offsetY} fontSize={14} text={box.indexS + 1} fill='white' strokeWidth={1} align="start" visible={box.speaker == this.state.speaker[0]} />}
+                                {false && <Text x={1} y={18} width={29} height={this.state.offsetY} fontSize={14} text={box.indexS + 1} fill='white' strokeWidth={1} align="start" visible={box.speaker == this.state.speaker[0]} />}
                                 
                                 {this.props.condition != 'slider' && [...Array(Math.abs(box.relative))].map((e, r_i) => { return <Arrow x= {7 * r_i + (box.indexS < 9 ? 14 : 22)} y= {31} points={[0, (box.relative < 0 ? -1 : 0) * 14, 0, (box.relative > 0 ? -1 : 0) * 14]}
                                     pointerLength ={4} pointerWidth={4} fill={'white'} stroke={'white'} strokeWidth={2} /> })}
