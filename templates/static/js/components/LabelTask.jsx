66f9391454118948516d3f3279288a806574c4c0
LawrenceZhou <hmskyijun@gmail.com>
2022-02-11 23:06:55 +0900

    turk, slider

--- templates/static/js/components/LabelTask.jsx
+++ templates/static/js/components/LabelTask.jsx
@@ -233,7 +233,7 @@ export default class LabelTask extends Component {
 			that.setState({nextConfirmOn: false});
 		}else{
 			//for user study
-			that.setState({sliderResults: {}, seenSentences: new Set(), nextConfirmOn: false, next: false, isStarted:false, lastTranscriptM:"", lastTranscriptF:"", currentTranscriptM:"", currentTranscriptF:"", currentIndexM:-1, currentIndexF:-1, atLeastOneRun:false, currentTaskIndex: that.state.currentTaskIndex + 1, speakerToLabel:that.state.taskList[that.state.currentTaskIndex + 1].speaker, dimensionToLabel: that.state.taskList[that.state.currentTaskIndex + 1].dimension});
+			that.setState({sliderResults: {}, sliderValue: 3, seenSentences: new Set(), nextConfirmOn: false, next: false, isStarted:false, lastTranscriptM:"", lastTranscriptF:"", currentTranscriptM:"", currentTranscriptF:"", currentIndexM:-1, currentIndexF:-1, atLeastOneRun:false, currentTaskIndex: that.state.currentTaskIndex + 1, speakerToLabel:that.state.taskList[that.state.currentTaskIndex + 1].speaker, dimensionToLabel: that.state.taskList[that.state.currentTaskIndex + 1].dimension});
 			//for pilot study
 			//that.setState({condition: "slider", nextConfirmOn: false, next: false, isStarted:false, lastTranscriptM:"", lastTranscriptF:"", currentTranscriptM:"", currentTranscriptF:"", currentIndexM:-1, currentIndexF:-1, atLeastOneRun:false, currentTaskIndex: that.state.currentTaskIndex + 1, speakerToLabel:that.state.taskList[that.state.currentTaskIndex + 1].speaker, dimensionToLabel: that.state.taskList[that.state.currentTaskIndex + 1].dimension});
 		}
